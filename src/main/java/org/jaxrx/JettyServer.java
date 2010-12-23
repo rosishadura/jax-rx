75a29311e0d496bee52f521b04eeb6f3e5df2904
lukas <lukas.lewandowski@inf.uni-konstanz.de>
2010-12-23 13:29:43 +0000

    NEW - task 406: Authentication https://projects.uni-konstanz.de/issues/show/406
    Preparing Security features, and evaluation. Working on 1.2.8-SNAPHSHOT

--- src/main/java/org/jaxrx/JettyServer.java
+++ src/main/java/org/jaxrx/JettyServer.java
@@ -2,56 +2,114 @@ package org.jaxrx;
 
 import org.jaxrx.core.JaxRxException;
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.security.Constraint;
+import org.mortbay.jetty.security.ConstraintMapping;
+import org.mortbay.jetty.security.HashUserRealm;
+import org.mortbay.jetty.security.SecurityHandler;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
+
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 /**
  * This is the main class to start the Jetty server to offer RESTful web
  * services support.
- *
+ * 
  * @author Sebastian Graf, Christian Gruen, Patrick Lang, Lukas Lewandowski,
  *         University of Konstanz
- *
+ * 
  */
 public final class JettyServer {
-	/**
-	 * Server reference.
-	 */
-	private transient Server server;
+  /**
+   * Server reference.
+   */
+  private transient Server server;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param port
-	 *            web server port
-	 * @throws Exception
-	 *            exception
-	 */
-	public JettyServer(final int port) throws Exception {
+  /**
+   * Constructor.
+   * 
+   * @param port
+   *          web server port
+   * @throws Exception
+   *           exception
+   */
+  public JettyServer(final int port) throws Exception {
     server = new Server(port);
-    final ServletHolder servHolder = new ServletHolder(
-        ServletContainer.class);
+
+    final ServletHolder servHolder = new ServletHolder(ServletContainer.class);
     servHolder.setInitParameter(
         "com.sun.jersey.config.property.resourceConfigClass",
         "com.sun.jersey.api.core.PackagesResourceConfig");
-    servHolder.setInitParameter(
-        "com.sun.jersey.config.property.packages",
+    servHolder.setInitParameter("com.sun.jersey.config.property.packages",
         "org.jaxrx.resource");
 
     final Context context = new Context(server, "/", Context.SESSIONS);
     context.addServlet(servHolder, "/");
     server.start();
-	}
+  }
 
-	/**
+  /**
+   * Constructor.
+   * 
+   * @param port
+   *          web server port
+   * @param authentication <code>true</code> if server has to authenticate,
+   *          <code>false</code> otherwise.
+   * @throws Exception
+   *           exception
+   */
+  public JettyServer(final int port, final boolean authentication)
+      throws Exception {
+    server = new Server(port);
+      
+    // create a new constraint for authentication
+    Constraint constraint = new Constraint();
+    // choose possible authentication, e.g., Basic, Digest, Form, etc
+    constraint.setName(Constraint.__BASIC_AUTH);
+    // set allowed roles
+    constraint.setRoles(new String[] { "user", "admin", "read-only"});
+    // set authentication as "must have", not authenticated user will not be
+    // accepted
+    constraint.setAuthenticate(authentication);
+
+    // mapping of constraint to resource
+    ConstraintMapping cm = new ConstraintMapping();
+    cm.setConstraint(constraint);
+    // users have to authenticate on all available resources
+    cm.setPathSpec("/*");
+
+    // choosing of security handler
+    SecurityHandler sh = new SecurityHandler();
+
+    // possible realms are HashUserRealm, JDBCUserRealm and JAASUserRealm
+    sh.setUserRealm(new HashUserRealm("MyRealm",
+        System.getProperty("user.home") + "/realm.properties"));
+    // add constraintmapping
+    sh.setConstraintMappings(new ConstraintMapping[] { cm});
+
+    // add security handler to server instance
+    server.addHandler(sh);
+
+    final ServletHolder servHolder = new ServletHolder(ServletContainer.class);
+    servHolder.setInitParameter(
+        "com.sun.jersey.config.property.resourceConfigClass",
+        "com.sun.jersey.api.core.PackagesResourceConfig");
+    servHolder.setInitParameter("com.sun.jersey.config.property.packages",
+        "org.jaxrx.resource");
+
+    final Context context = new Context(server, "/", Context.SESSIONS);
+    context.addServlet(servHolder, "/");
+    server.start();
+  }
+
+  /**
    * Stops the server.
    */
-	public void stop() {
-	  try {
+  public void stop() {
+    try {
       server.stop();
-    } catch (final Exception exce) {
+    } catch(final Exception exce) {
       throw new JaxRxException(exce);
     }
-	}
+  }
 }
--- src/main/java/org/jaxrx/resource/XMLResource.java
+++ src/main/java/org/jaxrx/resource/XMLResource.java
@@ -1,6 +1,7 @@
 package org.jaxrx.resource;
 
 import java.io.InputStream;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,7 +12,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
+
 import org.jaxrx.JaxRx;
 import org.jaxrx.core.ResourcePath;
 import org.jaxrx.core.Systems;
@@ -22,126 +25,144 @@ import org.jaxrx.core.URLConstants;
  * This means that JAX-RX returns the available resources which the underlying
  * implementation provide. Available resources could be collections or
  * particular XML resources.
- *
+ * 
  * @author Sebastian Graf, Christian Gruen, Lukas Lewandowski, University of
  *         Konstanz
- *
+ * 
  */
 @Path(URLConstants.RESOURCEPATH)
 public final class XMLResource extends AResource {
-	/**
-	 * This method returns a collection of available resources. An available
-	 * resource can be either a particular XML resource or a collection
-	 * containing further XML resources.
-	 *
-	 * @param system
-	 *            The associated system with this request.
-	 * @param resource
-	 *            The name of the requested resource.
-	 * @param uri
-	 *            The context information due to the requested URI.
-	 * @return A collection of available resources.
-	 */
-	@GET
-	public Response getResource(
-			@PathParam(URLConstants.SYSTEM) final String system,
-			@PathParam(URLConstants.RESOURCE) final String resource,
-			@Context final UriInfo uri) {
+  /**
+   * This method returns a collection of available resources. An available
+   * resource can be either a particular XML resource or a collection
+   * containing further XML resources.
+   * 
+   * @param system
+   *          The associated system with this request.
+   * @param resource
+   *          The name of the requested resource.
+   * @param uri
+   *          The context information due to the requested URI.
+   * 
+   * @param sec The security context used for the request.
+   * @return A collection of available resources.
+   */
+  @GET
+  public Response getResource(
+      @PathParam(URLConstants.SYSTEM) final String system,
+      @PathParam(URLConstants.RESOURCE) final String resource,
+      @Context final UriInfo uri, @Context final SecurityContext sec) {
 
-		return getResource(system, uri, resource);
-	}
+    if(sec != null) {
 
-	/**
-	 * This method will be called when a HTTP client sends a POST request to an
-	 * existing resource with 'application/query+xml' as Content-Type.
-	 *
-	 * @param system
-	 *            The implementation system.
-	 * @param resource
-	 *            The resource name.
-	 * @param input
-	 *            The input stream.
-	 * @return The {@link Response} which can be empty when no response is
-	 *         expected. Otherwise it holds the response XML file.
-	 */
-	@POST
-	@Consumes(APPLICATION_QUERY_XML)
-	public Response postQuery(
-			@PathParam(URLConstants.SYSTEM) final String system,
-			@PathParam(URLConstants.RESOURCE) final String resource,
-			final InputStream input) {
+      // the used security scheme (Basic, Digest, etc)
+      final String authChannel = sec.getAuthenticationScheme();
+      System.out.println("Authentication Scheme: " + authChannel);
+
+      // the authenticated user (authentication has been performed by server)
+      System.out.println("User principal: " + sec.getUserPrincipal().getName());
+
+      // this checks if the user is in a specific role, BUT unfortuntelly it is
+      // not working with Jetty, until now I have not found any workaround...
+      if(sec.isUserInRole("user")) {
+        System.out.println("User: " + sec.getUserPrincipal().getName()
+            + ", Go Baby Go!!! ");
+      }
+    }
+    return getResource(system, uri, resource);
+  }
+
+  /**
+   * This method will be called when a HTTP client sends a POST request to an
+   * existing resource with 'application/query+xml' as Content-Type.
+   * 
+   * @param system
+   *          The implementation system.
+   * @param resource
+   *          The resource name.
+   * @param input
+   *          The input stream.
+   * @return The {@link Response} which can be empty when no response is
+   *         expected. Otherwise it holds the response XML file.
+   */
+  @POST
+  @Consumes(APPLICATION_QUERY_XML)
+  public Response postQuery(
+      @PathParam(URLConstants.SYSTEM) final String system,
+      @PathParam(URLConstants.RESOURCE) final String resource,
+      final InputStream input) {
 
     return postQuery(system, input, resource);
-	}
+  }
 
-	/**
-	 * This method will be called when an HTTP client sends a POST request to an
-	 * existing resource to add a resource. Content-Type must be 'text/xml'.
-	 *
-	 * @param system
-	 *            The implementation system.
-	 * @param resource
-	 *            The resource name.
-	 * @param input
-	 *            The input stream.
-	 * @return The {@link Response} which can be empty when no response is
-	 *         expected. Otherwise it holds the response XML file.
-	 */
-	@POST
-  @Consumes({ MediaType.TEXT_XML, MediaType.APPLICATION_XML })
-	public Response postResource(
-			@PathParam(URLConstants.SYSTEM) final String system,
-			@PathParam(URLConstants.RESOURCE) final String resource,
-			final InputStream input) {
+  /**
+   * This method will be called when an HTTP client sends a POST request to an
+   * existing resource to add a resource. Content-Type must be 'text/xml'.
+   * 
+   * @param system
+   *          The implementation system.
+   * @param resource
+   *          The resource name.
+   * @param input
+   *          The input stream.
+   * @return The {@link Response} which can be empty when no response is
+   *         expected. Otherwise it holds the response XML file.
+   */
+  @POST
+  @Consumes({ MediaType.TEXT_XML, MediaType.APPLICATION_XML})
+  public Response postResource(
+      @PathParam(URLConstants.SYSTEM) final String system,
+      @PathParam(URLConstants.RESOURCE) final String resource,
+      final InputStream input) {
 
-		final JaxRx impl = Systems.getInstance(system);
-		final String info = impl.add(input, new ResourcePath(resource));
-		return Response.created(null).entity(info).build();
-	}
+    final JaxRx impl = Systems.getInstance(system);
+    final String info = impl.add(input, new ResourcePath(resource));
+    return Response.created(null).entity(info).build();
+  }
 
-	/**
-	 * This method will be called when a new XML file has to be stored within
-	 * the database. The user request will be forwarded to this method.
-	 * Afterwards it creates a response message with the 'created' HTTP status
-	 * code, if the storing has been successful.
-	 *
-	 * @param system
-	 *            The associated system with this request.
-	 * @param resource
-	 *            The name of the new resource.
-	 * @param xml
-	 *            The XML file as {@link InputStream} that will be stored.
-	 * @return The HTTP status code as response.
-	 */
-	@PUT
-  @Consumes({ MediaType.TEXT_XML, MediaType.APPLICATION_XML })
-	public Response putResource(
-			@PathParam(URLConstants.SYSTEM) final String system,
-			@PathParam(URLConstants.RESOURCE) final String resource,
-			final InputStream xml) {
+  /**
+   * This method will be called when a new XML file has to be stored within
+   * the database. The user request will be forwarded to this method.
+   * Afterwards it creates a response message with the 'created' HTTP status
+   * code, if the storing has been successful.
+   * 
+   * @param system
+   *          The associated system with this request.
+   * @param resource
+   *          The name of the new resource.
+   * @param xml
+   *          The XML file as {@link InputStream} that will be stored.
+   * @return The HTTP status code as response.
+   */
+  @PUT
+  @Consumes({ MediaType.TEXT_XML, MediaType.APPLICATION_XML})
+  public Response putResource(
+      @PathParam(URLConstants.SYSTEM) final String system,
+      @PathParam(URLConstants.RESOURCE) final String resource,
+      final InputStream xml) {
 
-		final JaxRx impl = Systems.getInstance(system);
-		final String info = impl.update(xml, new ResourcePath(resource));
-		return Response.created(null).entity(info).build();
-	}
+    final JaxRx impl = Systems.getInstance(system);
+    final String info = impl.update(xml, new ResourcePath(resource));
+    return Response.created(null).entity(info).build();
+  }
 
-	/**
-	 * This method will be called when an HTTP client sends a DELETE request to
-	 * delete an existing resource.
-	 *
-	 * @param system
-	 *            The associated system with this request.
-	 * @param resource
-	 *            The name of the existing resource that has to be deleted.
-	 * @return The HTTP response code for this call.
-	 */
-	@DELETE
-	public Response deleteResource(
-			@PathParam(URLConstants.SYSTEM) final String system,
-			@PathParam(URLConstants.RESOURCE) final String resource) {
+  /**
+   * This method will be called when an HTTP client sends a DELETE request to
+   * delete an existing resource.
+   * 
+   * @param system
+   *          The associated system with this request.
+   * @param resource
+   *          The name of the existing resource that has to be deleted.
+   * @return The HTTP response code for this call.
+   */
+  @DELETE
+  public Response deleteResource(
+      @PathParam(URLConstants.SYSTEM) final String system,
+      @PathParam(URLConstants.RESOURCE) final String resource) {
 
-		final JaxRx impl = Systems.getInstance(system);
-		final String info = impl.delete(new ResourcePath(resource));
-		return Response.ok().entity(info).build();
-	}
+    final JaxRx impl = Systems.getInstance(system);
+    final String info = impl.delete(new ResourcePath(resource));
+    return Response.ok().entity(info).build();
+  }
 }
